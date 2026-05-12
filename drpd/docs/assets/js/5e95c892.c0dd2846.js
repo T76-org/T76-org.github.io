61ae0e5a6f54e189de4c1ef6ab7424ed5dc8cd14
Marco Tabini <marcot@tabini.ca>
2026-05-12 09:59:18 -0400

    Doc release

---
+++ drpd/docs/assets/js/5e95c892.c0dd2846.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdrpd_docs=globalThis.webpackChunkdrpd_docs||[]).push([[647],{7121(s,e,r){r.r(e),r.d(e,{default:()=>h});r(6540);var d=r(4164),a=r(7559),c=r(5500),u=r(2831),l=r(1656),o=r(4848);function h(s){return(0,o.jsx)(c.e3,{className:(0,d.A)(a.G.wrapper.docsPages),children:(0,o.jsx)(l.A,{children:(0,u.v)(s.route.routes)})})}}}]);
---
+++ drpd/docs/assets/js/814f3328.d336755f.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdrpd_docs=globalThis.webpackChunkdrpd_docs||[]).push([[472],{5513(s){s.exports=JSON.parse('{"title":"Recent posts","items":[]}')}}]);
